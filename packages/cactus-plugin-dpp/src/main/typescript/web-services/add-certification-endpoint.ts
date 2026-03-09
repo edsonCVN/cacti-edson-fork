e73500b2cfe8254e35f0f40fbba3ee6877eb106f
Edson da Veiga <edson.da.veiga@tecnico.ulisboa.pt>
2026-03-09 14:22:33 +0000

    feat(dpp): add web-service endpoint handlers
    Signed-off-by: Edson da Veiga <edson.da.veiga@tecnico.ulisboa.pt>

---
+++ packages/cactus-plugin-dpp/src/main/typescript/web-services/add-certification-endpoint.ts
@@ -0,0 +1,102 @@
+import { Express, Request, Response } from "express";
+
+import {
+  Logger,
+  Checks,
+  LogLevelDesc,
+  LoggerProvider,
+  IAsyncProvider,
+  safeStringifyException,
+} from "@hyperledger/cactus-common";
+import {
+  IEndpointAuthzOptions,
+  IExpressRequestHandler,
+  IWebServiceEndpoint,
+} from "@hyperledger/cactus-core-api";
+import { registerWebServiceEndpoint } from "@hyperledger/cactus-core";
+
+import { PluginDPP } from "../plugin-dpp";
+
+import { AddCertificationRequest } from "./../generated/openapi/typescript-axios";
+
+import OAS from "../../json/openapi.json";
+
+export interface IAddCertificationEndpointOptions {
+  logLevel?: LogLevelDesc;
+  plugin: PluginDPP;
+}
+
+export class AddCertificationEndpoint implements IWebServiceEndpoint {
+  public static readonly CLASS_NAME = "AddCertificationEndpoint";
+  private readonly log: Logger;
+
+  public get className(): string {
+    return AddCertificationEndpoint.CLASS_NAME;
+  }
+
+  constructor(public readonly options: IAddCertificationEndpointOptions) {
+    const fnTag = `${this.className}#constructor()`;
+    Checks.truthy(options, `${fnTag} arg options`);
+    Checks.truthy(options.plugin, `${fnTag} arg options.plugin`);
+
+    const level = options.logLevel || "INFO";
+    const label = this.className;
+    this.log = LoggerProvider.getOrCreate({ level, label });
+  }
+
+  public get oasPath(): (typeof OAS.paths)["/api/v1/@hyperledger/cactus-plugin-dpp/add-certification"] {
+    return OAS.paths[
+      "/api/v1/@hyperledger/cactus-plugin-dpp/add-certification"
+    ];
+  }
+
+  public getPath(): string {
+    return this.oasPath.post["x-hyperledger-cacti"].http.path;
+  }
+
+  public getVerbLowerCase(): string {
+    return this.oasPath.post["x-hyperledger-cacti"].http.verbLowerCase;
+  }
+
+  public getOperationId(): string {
+    return this.oasPath.post.operationId;
+  }
+
+  getAuthorizationOptionsProvider(): IAsyncProvider<IEndpointAuthzOptions> {
+    return {
+      get: async () => ({
+        isProtected: true,
+        requiredRoles: [],
+      }),
+    };
+  }
+
+  public async registerExpress(
+    expressApp: Express,
+  ): Promise<IWebServiceEndpoint> {
+    await registerWebServiceEndpoint(expressApp, this);
+    return this;
+  }
+
+  public getExpressRequestHandler(): IExpressRequestHandler {
+    return this.handleRequest.bind(this);
+  }
+
+  private async handleRequest(req: Request, res: Response): Promise<void> {
+    const reqTag = `${this.getVerbLowerCase()} - ${this.getPath()}`;
+    this.log.debug(reqTag);
+
+    const reqBody = req.body as AddCertificationRequest;
+
+    try {
+      const resp = await this.options.plugin.addCertification(reqBody);
+      res.status(200).json(resp);
+    } catch (ex) {
+      this.log.error(`Crash while serving ${reqTag}`, ex);
+      res.status(500).json({
+        message: "Internal Server Error",
+        error: safeStringifyException(ex),
+      });
+    }
+  }
+}
